61cbe2b2d0bdc210dc0121ef3eed71c88412325c
ejgarciaq <edson.garcia.cr@outlook.com>
2025-08-18 16:13:24 -0600

    first commit

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from payroll_app import create_app
+
+# Llama a la funcion create_app para crear la instancia de la aplicación Flask
+app = create_app()
+
+# Ruta para la página de inicio
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Verifica si el script se está ejecutando directamente
+if __name__ == '__main__':
+    # Ejecuta la aplicación en modo de depuración
+    app.run(debug=True)
