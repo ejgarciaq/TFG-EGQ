47a8daf79f430354590f2ff0bcdc6e84f11e3bbd
ejgarciaq <edson.garcia.cr@outlook.com>
2025-09-09 10:47:42 -0600

    se arregla modelos por clave pendiente

--- payroll_app/models.py
+++ payroll_app/models.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from payroll_app import db
 from flask_login import UserMixin
 
-## Gestión Administrativa de la base de datos ##
+# Gestión Administrativa de la base de datos
 # Este archivo define la estructura de las tablas de la base de datos
 # a través de los modelos de SQLAlchemy, facilitando la interacción con la misma.
 
-#------- Roles ---------------------------------------------------
+# --- Roles ---
 
 class Rol(db.Model):
     """
@@ -14,17 +14,15 @@ class Rol(db.Model):
     Almacena los roles de usuario (ej. 'Administrador', 'Empleado').
     """
     __tablename__ = 'rol'
-    id_rol = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del rol
-    tipo_rol = db.Column(db.String(100), unique=True, nullable=False) # Campo para el nombre del rol, único y obligatorio
-    descripcion_rol = db.Column(db.String(255), nullable=True)     # Campo opcional para una descripción del rol
-    # Relación: Un rol puede tener muchos usuarios.
-    # 'back_populates' crea una relación bidireccional con el modelo Usuario.
+    id_rol = db.Column(db.Integer, primary_key=True)
+    tipo_rol = db.Column(db.String(100), unique=True, nullable=False)
+    descripcion_rol = db.Column(db.String(255), nullable=True)
     usuarios = db.relationship('Usuario', back_populates='rol')
     
     def __repr__(self):
         return f'<Rol {self.tipo_rol}>'
     
-#------ Usuarios -------------------------------------------------
+# --- Usuarios ---
 
 class Usuario(db.Model, UserMixin):
     """
@@ -33,18 +31,17 @@ class Usuario(db.Model, UserMixin):
     'UserMixin' añade propiedades y métodos para Flask-Login.
     """
     __tablename__ = 'usuario'
-    id_usuario = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del usuario
-    username = db.Column(db.String(50), unique=True, nullable=False)  # Nombre de usuario, único y obligatorio
-    password = db.Column(db.String(255), nullable=False) # Contraseña del usuario, almacenada como hash
-    estado_usuario = db.Column(db.Boolean, nullable=False) # Estado del usuario (activo/inactivo)
-    # Clave foránea que relaciona un usuario con un rol
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    estado_usuario = db.Column(db.Boolean, nullable=False)
     Rol_id_rol = db.Column(db.Integer, db.ForeignKey('rol.id_rol'), nullable=False)
-    # Relación bidireccional con la tabla 'rol'
+    
+    # ✅ Relaciones bidireccionales
     rol = db.relationship('Rol', back_populates='usuarios')
-    # Relación bidireccional con la tabla 'empleado'
-    # 'uselist=False' indica que un usuario solo puede estar asociado a un empleado.
     empleado = db.relationship('Empleado', back_populates='usuario', uselist=False)
-    # Campos de seguridad para la gestión de la cuenta
+    
+    # Campos de seguridad
     intentos_fallidos = db.Column(db.Integer, default=0, nullable=False)
     fecha_ultimo_intento = db.Column(db.DateTime, nullable=True)
     cambio_password_requerido = db.Column(db.Boolean, default=False, nullable=False)
@@ -56,7 +53,7 @@ class Usuario(db.Model, UserMixin):
     def __repr__(self):
         return f'<Usuario {self.username}>'
     
-#------- Puesto ---------------------------------------------------    
+# --- Puesto ---
     
 class Puesto(db.Model):
     """
@@ -64,14 +61,14 @@ class Puesto(db.Model):
     Almacena los diferentes puestos de trabajo en la empresa.
     """
     __tablename__ = 'puesto'
-    id_puesto = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del puesto
-    tipo_puesto = db.Column(db.String(100), unique=True, nullable=False) # Nombre del puesto, único y obligatorio
-    empleados = db.relationship('Empleado', back_populates='puesto') # Relación: Un puesto puede tener muchos empleados
+    id_puesto = db.Column(db.Integer, primary_key=True)
+    tipo_puesto = db.Column(db.String(100), unique=True, nullable=False)
+    empleados = db.relationship('Empleado', back_populates='puesto')
 
     def __repr__(self):
         return f'<Puesto {self.tipo_puesto}>'
     
-#----- Empleados ---------------------------------------------------------
+# --- Empleados ---
     
 class Empleado(db.Model):
     """
@@ -79,7 +76,8 @@ class Empleado(db.Model):
     Almacena la información personal y laboral de cada empleado.
     """
     __tablename__ = 'empleado'
-    id_empleado = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del empleado
+    id_empleado = db.Column(db.Integer, primary_key=True)
+    
     # Datos personales
     nombre = db.Column(db.String(100), nullable=False)
     apellido_primero = db.Column(db.String(100), nullable=False)
@@ -87,18 +85,24 @@ class Empleado(db.Model):
     cedula = db.Column(db.String(20), unique=True, nullable=False)
     correo = db.Column(db.String(100), unique=True, nullable=False)
     telefono = db.Column(db.String(15), nullable=False)
+    
     # Datos laborales
     fecha_ingreso = db.Column(db.Date, nullable=False)
     salario_base = db.Column(db.Float, nullable=False)
     fecha_salida = db.Column(db.Date, nullable=True)
     estado_empleado = db.Column(db.Boolean, nullable=False)
-    # Claves foráneas que lo relacionan con otras tablas
+    
+    # Claves foráneas
     Puesto_id_puesto = db.Column(db.Integer, db.ForeignKey('puesto.id_puesto'), nullable=False)
-    Usuario_id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), unique=True, nullable=False)
     TipoNomina_id_tipo_nomina = db.Column(db.Integer, db.ForeignKey('tipo_nomina.id_tipo_nomina'), nullable=False)
+    Usuario_id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), unique=True, nullable=False)
+
     # Control de vacaciones
     vacaciones_disponibles = db.Column(db.Integer, default=0, nullable=False)
-    # Relaciones para acceder a los datos de nómina y asistencia
+    
+    # Relaciones
+    usuario = db.relationship('Usuario', back_populates='empleado')
+    puesto = db.relationship('Puesto', back_populates='empleados')
     nominas = db.relationship('Nomina', back_populates='empleado')
     registros_asistencia = db.relationship('RegistroAsistencia', backref='empleado', lazy=True)
     tipo_nomina_relacion = db.relationship('TipoNomina', back_populates='empleados_relacionados', lazy=True)
@@ -113,7 +117,7 @@ class Empleado(db.Model):
     def __repr__(self):
         return f'<Empleado {self.nombre} {self.apellido_primero}>'
     
-#----------------- Feriados -------------------------------------------
+# --- Feriados ---
 
 class Feriado(db.Model):
     """
@@ -121,16 +125,16 @@ class Feriado(db.Model):
     Almacena los días feriados o festivos.
     """
     __tablename__ = 'feriado'
-    id_feriado = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del feriado
-    fecha_feriado = db.Column(db.Date, unique=True, nullable=False)  # Fecha del feriado, única y obligatoria
-    descripcion_feriado = db.Column(db.String(255), nullable=True) # Descripción del feriado
-    pago_obligatorio = db.Column(db.Boolean, nullable=False, default=False) # Indica si el feriado es de pago obligatorio
-    registros_asistencia = db.relationship('RegistroAsistencia', back_populates='feriado_relacion') # Relación con los registros de asistencia en días feriados
+    id_feriado = db.Column(db.Integer, primary_key=True)
+    fecha_feriado = db.Column(db.Date, unique=True, nullable=False)
+    descripcion_feriado = db.Column(db.String(255), nullable=True)
+    pago_obligatorio = db.Column(db.Boolean, nullable=False, default=False)
+    registros_asistencia = db.relationship('RegistroAsistencia', back_populates='feriado_relacion')
 
     def __repr__(self):
         return f'<Feriado {self.fecha_feriado}>'
 
-#----------- Tipo de nomina --------------------------------------
+# --- Tipo de nomina ---
 
 class TipoNomina(db.Model):
     """
@@ -138,19 +142,15 @@ class TipoNomina(db.Model):
     Clasifica los tipos de nómina (ej. quincenal, mensual).
     """
     __tablename__ = 'tipo_nomina'
-    id_tipo_nomina = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del tipo de nómina
-    nombre_tipo = db.Column(db.String(50), nullable=False) # Nombre del tipo de nómina
-
-    # Relación: Un tipo de nómina puede tener muchas nóminas asociadas
+    id_tipo_nomina = db.Column(db.Integer, primary_key=True)
+    nombre_tipo = db.Column(db.String(50), nullable=False)
     nominas_relacionadas = db.relationship('Nomina', back_populates='tipo_nomina_relacion', lazy=True)
-
-    # Relación: Un tipo de nómina puede tener muchos empleados asociados
     empleados_relacionados = db.relationship('Empleado', back_populates='tipo_nomina_relacion', lazy=True)
 
     def __repr__(self):
         return f'<TipoNomina {self.nombre_tipo}>'
 
-#--------- Nomina -------------------------------------------------
+# --- Nomina ---
 
 class Nomina(db.Model):
     """
@@ -158,21 +158,16 @@ class Nomina(db.Model):
     Almacena los registros de cálculo de nómina para un empleado en un periodo.
     """
     __tablename__ = 'nomina'
-    id_nomina = db.Column(db.Integer, primary_key=True) # Columna principal: ID único de la nómina
-    # Período de la nómina
-    fecha_inicio = db.Column(db.Date, nullable=False) 
+    id_nomina = db.Column(db.Integer, primary_key=True)
+    fecha_inicio = db.Column(db.Date, nullable=False)
     fecha_fin = db.Column(db.Date, nullable=False)
-    # Cálculos de pago
     salario_bruto = db.Column(db.Float, nullable=False)
     salario_neto = db.Column(db.Float, nullable=False)
     deducciones = db.Column(db.Float, nullable=False, default=0.0)
-    # Estado de pago
     pago_obligatorio = db.Column(db.Boolean, nullable=False, default=False)
     fecha_creacion = db.Column(db.DateTime, nullable=False)
-    # Claves foráneas
     Empleado_id_empleado = db.Column(db.Integer, db.ForeignKey('empleado.id_empleado'), nullable=False)
     TipoNomina_id_tipo_nomina = db.Column(db.Integer, db.ForeignKey('tipo_nomina.id_tipo_nomina'), nullable=False)
-    # Relaciones
     empleado = db.relationship('Empleado', back_populates='nominas')
     registros_asistencia = db.relationship('RegistroAsistencia', back_populates='nomina_relacion')
     tipo_nomina_relacion = db.relationship('TipoNomina', back_populates='nominas_relacionadas', lazy=True)
@@ -180,7 +175,7 @@ class Nomina(db.Model):
     def __repr__(self):
         return f'<Nomina {self.id_nomina} del Empleado {self.Empleado_id_empleado}>'
     
-#-------- Registro de Asistencia -----------------------------
+# --- Registro de Asistencia ---
     
 class RegistroAsistencia(db.Model):
     """
@@ -188,65 +183,54 @@ class RegistroAsistencia(db.Model):
     Almacena los registros diarios de entrada y salida de los empleados.
     """
     __tablename__ = 'registro_asistencia'
-    id_registro_asistencia = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del registro
-    # Campos de registro de tiempo
+    id_registro_asistencia = db.Column(db.Integer, primary_key=True)
     fecha_registro = db.Column(db.Date, nullable=False)
     hora_entrada = db.Column(db.Time, nullable=False)
     hora_salida = db.Column(db.Time, nullable=True)
-    # Campos de cálculo de horas y pago
     total_horas = db.Column(db.Float, nullable=True)
     monto_pago = db.Column(db.Float, nullable=True)
     hora_nominal = db.Column(db.Float, nullable=True)
     hora_extra = db.Column(db.Float, nullable=True)
     hora_feriado = db.Column(db.Float, nullable=True)
-    # Estado de aprobación
     aprobacion_registro = db.Column(db.Boolean, nullable=False, default=False)
-    # Claves foráneas
     Nomina_id_nomina = db.Column(db.Integer, db.ForeignKey('nomina.id_nomina'), nullable=True)
     Feriado_id_feriado = db.Column(db.Integer, db.ForeignKey('feriado.id_feriado'), nullable=True)
     Empleado_id_empleado = db.Column(db.Integer, db.ForeignKey('empleado.id_empleado'), nullable=False)
-
-    # Relaciones
     feriado_relacion = db.relationship('Feriado', back_populates='registros_asistencia')
     nomina_relacion = db.relationship('Nomina', back_populates='registros_asistencia')
 
     def __repr__(self):
         return f'<RegistroAsistencia {self.id_registro_asistencia}>'
     
-#--------- Aguinaldo -------------------------------------------------   
+# --- Aguinaldo ---
 class Aguinaldo(db.Model):
     """
     Modelo para la tabla que almacena el cálculo y el registro del aguinaldo.
     """
     __tablename__ = 'aguinaldo'
-    id_aguinaldo = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del aguinaldo
-    # Fecha y monto del pago        
+    id_aguinaldo = db.Column(db.Integer, primary_key=True)
     fecha_pago = db.Column(db.Date, nullable=False)
     monto = db.Column(db.Float, nullable=False)
-    
-    # Clave foránea que enlaza el aguinaldo con el empleado
     Empleado_id_empleado = db.Column(db.Integer, db.ForeignKey('empleado.id_empleado'), nullable=False)
-    
-    # Relación para acceder al objeto Empleado
     empleado_relacion = db.relationship('Empleado', backref='aguinaldos', lazy=True)
     
     def __repr__(self):
         return f'<Aguinaldo {self.id_aguinaldo} para Empleado {self.Empleado_id_empleado}>' 
-       
-#--------- Tipo de Acción de Personal --------------------------------
+        
+# --- Tipo de Acción de Personal ---
 class Tipo_AP(db.Model):
     """
     Modelo para la tabla que clasifica los tipos de acción de personal.
     """
     __tablename__ = 'tipo_ap'
-    id_tipo_ap = db.Column(db.Integer, primary_key=True) # Columna principal: ID único del tipo de acción
-    nombre_tipo = db.Column(db.String(100), unique=True, nullable=False) # Nombre del tipo de acción, único y obligatorio
-    descripcion_tipo = db.Column(db.String(255), nullable=True) # Descripción del tipo de acción
+    id_tipo_ap = db.Column(db.Integer, primary_key=True)
+    nombre_tipo = db.Column(db.String(100), unique=True, nullable=False)
+    descripcion_tipo = db.Column(db.String(255), nullable=True)
 
     def __repr__(self):
         return f'<Tipo_AP {self.nombre_tipo}>'
     
-#--------- Acción de Personal ----------------------------------------
+# --- Acción de Personal ---
     
 class Accion_Personal(db.Model):
     """
@@ -255,29 +239,18 @@ class Accion_Personal(db.Model):
     """
     __tablename__ = 'accion_personal'
     
-    #  Clave principal
-    id_accion = db.Column(db.Integer, primary_key=True) # Columna principal de identificación
-    
-    #  Claves foráneas
+    id_accion = db.Column(db.Integer, primary_key=True)
     Empleado_id_empleado = db.Column(db.Integer, db.ForeignKey('empleado.id_empleado'), nullable=False)
     Tipo_Ap_id_tipo_ap = db.Column(db.Integer, db.ForeignKey('tipo_ap.id_tipo_ap'), nullable=False)
-    
-    # Campos para registrar la solicitud
     fecha_accion = db.Column(db.Date, nullable=False, default=datetime.utcnow)
     detalles = db.Column(db.Text, nullable=True)
-    
-    # Campos específicos para solicitudes de tiempo
     cantidad_dia = db.Column(db.Integer, nullable=True)
     fecha_inicio = db.Column(db.Date, nullable=True)
     fecha_fin = db.Column(db.Date, nullable=True)
-    
-    #  Campos para el flujo de aprobación
-    estado_ap = db.Column(db.Integer, nullable=False, default=1)  # 1=Pendiente, 2=Aprobado, 3=Rechazado
-    id_aprobador = db.Column(db.Integer, nullable=True) # ID del usuario que aprueba
+    estado_ap = db.Column(db.Integer, nullable=False, default=1)
+    id_aprobador = db.Column(db.Integer, nullable=True)
     fecha_aprobacion = db.Column(db.DateTime, nullable=True)
-    
-    #  Campo para documentos adjuntos
-    documento_adjunto = db.Column(db.Text, nullable=True) # Guarda la ruta o URL del archivo
+    documento_adjunto = db.Column(db.Text, nullable=True)
     
     # Relaciones de SQLAlchemy
     empleado = db.relationship('Empleado', backref='acciones_personales', lazy=True)
